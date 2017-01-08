114d19b1a47201568b62c66db376c0c027f936b4
Waleed Ashraf <waleedashraf@outlook.com>
2017-01-08 23:14:58 +0500

    app to use passport/bodyParser/routes

--- server.js
+++ server.js
@@ -3,19 +3,19 @@
 import path from 'path';
 import { Server } from 'http';
 import Express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { match, RouterContext } from 'react-router';
-import routes from './web/routes';
+import models from './models';
 import logger from './logger.js';
 import morgan from 'morgan';
+import passport from 'passport';
+import session from 'express-session';
+import bodyParser from 'body-parser';
+import config from './config';
 
 // initialize the server and configure support for ejs templates
 const app = new Express();
 const server = new Server(app);
 const env = process.env.NODE_ENV || 'development';
 const port = process.env.PORT || 3000;
-var router = Express.Router();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'web', 'views'));
@@ -23,67 +23,51 @@ app.set('views', path.join(__dirname, 'web', 'views'));
 // define the folder that will be used for static assets
 app.use(Express.static(path.join(__dirname, 'static')));
 
-// use router
-app.use('/', router);
+// pass passport for configuration
+require('./lib/passport')(passport);
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: config.env.sessionSecret
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // override express logger
 logger.debug("Overriding 'Express' logger");
-app.use(morgan('common',{ "stream": logger.stream }));
+app.use(morgan('common', { "stream": logger.stream }));
 
 // use SSL
 app.use(requireHTTPS);
 
-// universal routing and rendering
-router.get('/', (req, res) => {
-    match(
-        { routes, location: req.url },
-        (err, redirectLocation, renderProps) => {
+// routes, load our routes and pass in our app and fully configured passport
+require('./lib/routes.js')(app, passport); 
 
-            // in case of error display the error message
-            if (err) {
-                logger.error("err page");
-                return res.status(500).send(err.message);         
-            }
+// sequelize sync models
+models.sequelize.sync().then(function () {
 
-            // in case of redirect propagate the redirect to the browser
-            if (redirectLocation) {
-                logger.info("react redirect");
-                return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-            }
-
-            // generate the React markup for the current route
-            let markup;
-            if (renderProps) {
-                // if the current route matched we have renderProps
-                markup = renderToString(<RouterContext {...renderProps} />);
-                logger.info("markup render" , req.url);
-                return res.render('index', { markup });
-            }else{
-                return;
-            }
-        }
-    );
-});
-
-// app.get( '/abc' , (req, res) => {
-  
-//   res.render('pageNotFound');  
-// });
-
-// start the server
-server.listen(port, err => {
+  // start express server
+  server.listen(port, err => {
     if (err) {
-        return logger.error(err);
+      return logger.error(err);
     }
     logger.info(`Server running on http://localhost:${port} [${env}]`);
+  });
+}).error(function (error) {
+  logger.error("ERROR IN CONN : " + error)
 });
 
 function requireHTTPS(req, res, next) {
-    if (req.headers['x-forwarded-proto'] != 'https' && env == 'production') {
-        return res.redirect(301, ['https://', req.get('Host'), req.url].join(''));
-    }
-    res.setHeader("X-Powered-By", "caari");
-    next();
+  if (req.headers['x-forwarded-proto'] != 'https' && env == 'production') {
+    return res.redirect(301, ['https://', req.get('Host'), req.url].join(''));
+  }
+  res.setHeader("X-Powered-By", "caari");
+  next();
 }
 
-module.exports = router;
+module.exports = app;
